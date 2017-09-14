e749f27afaf11c32c1e77c72e5e62eb8c256c446
tong <tong@disktree.net>
2017-09-15 00:32:11 +0200

    No more meta just one App.context (ctx)

--- src/om/App.hx
+++ src/om/App.hx
@@ -1,105 +1,153 @@
 package om;
 
-#if !macro
-@:autoBuild(om.App.complete())
-interface App {}
-#else
+import om.app.Platform;
 
-import Sys.println;
-import sys.FileSystem.*;
-import sys.io.File;
+#if macro
+import haxe.Template;
 import haxe.format.JsonPrinter;
 import haxe.macro.Compiler;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import om.Res;
 import om.style.LessC;
-
+import sys.FileSystem.*;
+import sys.FileSystem;
+import sys.io.File;
+import Sys.println;
 using om.Path;
+#end
 
 typedef Ctx = {
     var name : String;
     var version : String;
     var platform : Platform;
-    //var description : String;
+    var build : {
+        var num : Int;
+        var total : Int;
+        var time : Float;
+        var duration : Float;
+        var dev : Bool;
+        var release : Bool;
+    }
 };
 
-typedef Build = {
-    var num : Int;
-    var total : Int;
-    var date : Date;
-    var duration : Float;
-    var dev : Bool;
-    var release : Bool;
-}
-
-typedef Meta = {
-    var ctx : Ctx;
-    var build : Build;
-}
-
+#if !macro
+@:autoBuild(om.App.complete())
+#end
 class App {
 
-    static var ctx : Ctx;
+    public static inline var DEFINE_PREFIX = 'om_';
 
-    static function build() {
+    macro public static function getContext() : ExprOf<Ctx>
+        return macro $v{ctx};
+
+    #if macro
+
+    public static var ctx(default,null) : Ctx;
+
+    static function build( ?ctx_ : Ctx ) {
+
+        ctx = ctx_;
+        Template.globals = ctx;
 
         var ts_start = Time.stamp();
+        var path = Sys.getCwd().directory();
+        var dir = path.withoutDirectory();
 
-        ctx = {
-            name: getDefine( 'name', Sys.getCwd().directory().withoutDirectory() ),
-            version: getDefine( 'version', '0.0.0' ),
-            platform: getDefine( 'platform' ),
-        };
-        if( ctx.platform == null ) ctx.platform =
-            Context.defined( 'om_electron' ) ? electron :
-            Context.defined( 'om_android' ) ? android :
-            web;
-
-        var dev = getDefine( 'dev', false );
-        var release = getDefine( 'release', false );
-
-        var meta : Meta = null;
-        if( exists( '.om/build.json' ) ) {
-            meta = Json.parse( File.getContent( '.om/build.json' ) );
-            meta.ctx = ctx;
-            meta.build.total++;
-            if( meta.ctx.version < ctx.version ) {
-                meta.build.num = 0;
-            } else {
-                meta.build.num++;
-            }
-        } else {
-            meta = {
-                ctx: ctx,
-                build: { num: 0, total: 0, date: null, duration: 0.0, dev: dev, release: release }
+        if( ctx == null ) {
+            ctx = {
+                name:  getDefine( 'name', dir ),
+                version: getDefine( 'version', '0.0.0' ),
+                platform: getDefine( 'platform' ),
+                build: {
+                    num: 0,
+                    total: 0,
+                    duration: 0,
+                    time: Date.now().getTime(),
+                    dev: isDefined( 'dev', getDefine( 'dev', false, false ) ),
+                    release: isDefined( 'om_release', isDefined( 'release', false, false ) ),
+                }
             };
         }
+        if( ctx.platform == null ) {
+            ctx.platform =
+                Context.defined( 'om_electron' ) ? electron :
+                Context.defined( 'om_android' ) ? android :
+                web;
+        }
 
-        if( !exists( '.om' ) ) createDirectory( '.om' );
+        Compiler.define( 'name' );
+        Compiler.define( 'version', ctx.version );
+        Compiler.define( 'platform', ctx.platform );
+        switch ctx.platform {
+        case android: Compiler.define( 'android' );
+        case chrome: Compiler.define( 'chrome' );
+        case electron: Compiler.define( 'electron' );
+        case nme: Compiler.define( 'nme' );
+        case web: Compiler.define( 'web' );
+        }
+        if( ctx.build.dev ) Compiler.define( 'dev' );
+        if( ctx.build.release ) Compiler.define( 'release' );
+
+        if( !exists( '.om' ) ) createDirectory( '.om' )
+        else if( exists( '.om/context.json' ) ) {
+            var meta = Json.parse( File.getContent( '.om/context.json' ) );
+            ctx.build.num = meta.build.num;
+            ctx.build.total = meta.build.num;
+            if( meta.version < ctx.version ) ctx.build.num = 0;
+        }
+        ctx.build.num++;
+        ctx.build.total++;
+        ctx.build.duration = Time.stamp() - ts_start;
 
         Context.onAfterGenerate( function() {
 
+            ctx.build.duration = Time.stamp() - ts_start;
             buildResources();
+            ctx.build.duration = Time.stamp() - ts_start;
 
-            var tsEnd = Time.stamp();
+            File.saveContent( '.om/context.json', JsonPrinter.print( ctx, '\t' ) );
 
-            meta.build.duration = tsEnd - ts_start;
-            meta.build.date = Date.now();
-
-            File.saveContent( '.om/build.json', JsonPrinter.print( meta, '\t' ) );
-
-            println( '${ctx.name}-${ctx.platform}-${ctx.version}.'+meta.build.num );
+            println( '${ctx.name}-${ctx.platform}-${ctx.version}-'+ctx.build.num );
         });
     }
 
-    static inline function warn( info : String, ?pos : Position )
-        Context.warning( info, (pos == null) ? Context.currentPos() : pos );
-
-    static function getDefine<T>( key : String, ?def : T ) : T {
-        var k = 'om_app_$key';
-        return Context.defined( k ) ? cast Context.definedValue( k ) : def;
+    static function complete() {
+        var fields = Context.getBuildFields();
+        var pos = Context.currentPos();
+        //if( !fields.hasField( 'main' ) ) {
+        /*
+        if( !meta.build.release ) {
+            fields.push({
+                access: [APublic,AStatic],
+                name: '__app__',
+                kind: FVar( macro:Dynamic, macro $v{meta} ),
+                pos: pos
+            });
+            var info = '${ctx.name}-${ctx.platform}-${ctx.version}.'+meta.build.num;
+            fields.push({
+                access: [APublic,AStatic,AInline],
+                name: '__init__',
+                kind: FFun( { args:[], expr: macro console.info( $v{info} ), ret: null } ),
+                pos: pos
+            });
+        }
+        */
+        return fields;
     }
+
+    static function isDefined( key : String, default_ = false, prefix = true ) : Bool {
+        var k = prefix ? '$DEFINE_PREFIX$key' : key;
+        return Context.defined( k ) ? true : default_;
+    }
+
+    static function getDefine<T>( key : String, ?default_ : T, prefix = true ) : T {
+        var k = prefix ? '$DEFINE_PREFIX$key' : key;
+        return Context.defined( k ) ? cast Context.definedValue( k ) : default_;
+    }
+
+    static function warn( info : String, ?pos : Position )
+        Context.warning( info, (pos == null) ? Context.currentPos() : pos );
 
     static function buildResources() {
 
@@ -109,11 +157,16 @@ class App {
         syncDirectory( 'res/mesh', 'bin/mesh' );
         syncDirectory( 'res/script', 'bin/script' );
         syncDirectory( 'res/sound', 'bin/sound' );
-        syncDirectory( 'res/'+ctx.platform, 'bin' );
+        //syncDirectory( 'res/'+ctx.platform, 'bin' );
 
-        LessC.compileFile( 'res/style/app.less', 'bin/app.css' );
+        var lessFile = 'res/style/app.less';
+        if( exists( lessFile ) ) LessC.compileFile( lessFile, 'bin/app.css' );
 
-        syncFile( 'res/html/app.html', 'bin/index.html' );
+        var htmlFile = 'res/html/app.html';
+        if( exists( htmlFile ) ) {
+            //Context.registerModuleDependency();
+            syncFile( htmlFile, 'bin/index.html' );
+        }
     }
 
     static function isValidResName( fileName : String ) : Bool {
@@ -167,11 +220,16 @@ class App {
     }
 
     static function executeTemplate( src : String, ?ctx : Dynamic ) : String {
-        if( ctx == null ) ctx = App.ctx;
-        return new Template( File.getContent( src ) ).execute( ctx );
+        if( ctx == null ) ctx = om.util.ReflectUtil.createCopy( App.ctx );
+        Reflect.setField( ctx, ctx.platform, true );
+        var tpl = new Template( File.getContent( src ) );
+        return try tpl.execute( ctx ) catch(e:Dynamic) {
+            trace( e );
+            null;
+        }
     }
 
-    static function writeFile( path : String, content : String ) {
+    static function writeFile( path : String, content = '' ) {
         var f = File.write( path );
         f.writeString( content );
         f.close();
@@ -183,21 +241,6 @@ class App {
     static inline function fileModTime( path : String ) : Float
         return stat( path ).mtime.getTime();
 
-    static function complete() {
-        var fields = Context.getBuildFields();
-		var pos = Context.currentPos();
-        //TODO
-        return fields;
-    }
-}
+    #end
 
-#end
-
-@:enum abstract Platform(String) from String to String {
-    var android = 'android';
-    //var atom = 'atom';
-    var chrome = 'chrome';
-    var electron = 'electron';
-    var nme = 'nme';
-    var web = 'web';
 }
---
+++ src/om/app/Platform.hx
@@ -0,0 +1,10 @@
+package om.app;
+
+@:enum abstract Platform(String) from String to String {
+    var android = 'android';
+    //var atom = 'atom';
+    var chrome = 'chrome';
+    var electron = 'electron';
+    var nme = 'nme';
+    var web = 'web';
+}
